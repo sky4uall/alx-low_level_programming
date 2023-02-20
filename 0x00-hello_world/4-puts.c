4bd542cf9cb1fc9be766052b981ad565dd60e250
sky4uall <skythehero2013@hotmail.com>
2023-02-20 14:44:50 -0800

    puts 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
@@ -10,7 +10,7 @@
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 
 }
